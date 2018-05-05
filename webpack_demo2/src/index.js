0ff01ab5318c4e0345adfe5f41412ddc31b90fc1
XLILT <xiaolongma002@163.com>
2018-05-05 15:28:45 +0800

    webpack demo 2

---
+++ webpack_demo2/src/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const axios = require('axios');
+
+axios({
+  method: 'get',
+  url: '/test_proxy'  
+}).then(res => {
+	console.log(res.data);
+	//document.getElementById('includeHtml').innerHTML = res.data;
+	document.body.innerHTML = res.data;
+}).catch(err => {
+	console.log(err);
+});
---
+++ webpack_demo2/webpack_cfg/webpack.common.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+//const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, "../"),
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    //new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+    //new webpack.HashedModuleIdsPlugin()
+  ],
+  output: {
+    //filename: '[name].[chunkhash].js',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {    
+    modules: [
+      "../node_modules",
+      path.resolve(__dirname, "../src")
+    ],    
+    extensions: [".js", ".json", ".jsx", ".css"]
+  }
+};
