5c2a3a230279792265d093c6259a4b1c1d1010b6
XLILT <xiaolongma002@163.com>
2018-05-06 00:02:52 +0800

    antd form

---
+++ antd_demo/webpack_cfg/webpack.common.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, "../"),
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new webpack.HashedModuleIdsPlugin()
+  ],
+  output: {
+    filename: '[name].[chunkhash].js',    
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+            loader: 'babel-loader',            
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'less-loader', options: { javascriptEnabled: true } }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {    
+    modules: [
+      "../node_modules",
+      path.resolve(__dirname, "../src")
+    ],    
+    extensions: [".js", ".json", ".jsx", ".css"]
+  }
+};
