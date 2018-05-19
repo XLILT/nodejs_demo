24ab553db91e76e3c6e8effd60d45c991e1503e4
XLILT <xiaolongma002@163.com>
2018-05-19 13:33:58 +0800

    webpack

--- webpack_demo2/webpack_cfg/webpack.common.js
+++ webpack_demo2/webpack_cfg/webpack.common.js
@@ -25,10 +25,25 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        use: {
+            loader: 'babel-loader',            
+        },
+        exclude: /node_modules/
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
           'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'less-loader', options: { javascriptEnabled: true } }
         ]
       },
       {
